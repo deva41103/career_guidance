a1e259baf651efddb49e56b22030c1d0a2fc135e
devashishdhumal <devashishdhumal@gmail.com>
2026-04-09 23:49:28 +0530

    new 1

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ Main entry point
 
 from flask import Flask
 from flask_cors import CORS
+import os
 from routes.guidance import guidance_bp
 from routes.web import web_bp
 
@@ -24,4 +25,5 @@ def health():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
